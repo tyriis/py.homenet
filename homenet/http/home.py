a912c373147396cec289e0c9d64222096b7ab480
Nils Müller <nils@mueller.name>
2016-08-08 15:30:41 +0200

    removed no longer needed blog article db model and routes, added base yaml for db reset

--- homenet/http/home.py
+++ homenet/http/home.py
@@ -3,11 +3,5 @@ import homenet.db as db
 
 @router.route('home', '/', tpl='home.jinja2')
 def home(ctx):
-    return {
-        'articles': ctx.db.query(db.Article)
-    }
-    #html = ''
-    #for article in ctx.db.query(db.Article):
-    #    url = ctx.url('article', article)
-    #    html += '<a href="%s">%s</a><br>' % (url, article.title)
-    #return html
+    return {}
+
