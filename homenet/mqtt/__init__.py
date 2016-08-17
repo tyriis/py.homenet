801840e08a771326604524cffccacc790d8d9e43
Nils Müller <nils@mueller.name>
2016-08-17 21:16:58 +0200

    subscribe to multiple mqtt channels based on configured nodes

--- homenet/mqtt/__init__.py
+++ homenet/mqtt/__init__.py
@@ -1,1 +1,10 @@
 from .mqtt_handler import msg_handler
+
+def subscribe(ctx, mqtt):
+    with ctx.Context() as ctx:
+        from homenet import db
+        nodes = ctx.db.query(db.Node).all()
+        for node in nodes:
+            mqtt.subscribe(node.identifier, msg_handler)
+
+
