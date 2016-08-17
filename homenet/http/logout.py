de107cc4894bb18601ced7f04d50e6c4857f0bc3
Nils Müller <nils@mueller.name>
2016-08-17 21:29:54 +0200

    added logout page

---
+++ homenet/http/logout.py
@@ -0,0 +1,11 @@
+from ._router import router
+
+
+@router.route('logout', '/logout')
+def logout(ctx):
+    ctx.user = None
+    request = ctx.http.request
+    ctx.http.response.status_code = 302
+    ctx.http.response.location = '/'
+    ctx.http.response.set_cookie('session', '', max_age=0)
+    return ctx.http.response
