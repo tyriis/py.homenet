aa1d542d3301d74c69203f99cc1df6cc31550354
jazzlyn <jasmin.zesch@gmail.com>
2016-08-19 14:41:25 +0200

    rewrote modules list and details

--- homenet/tpl/components/location/details.js
+++ homenet/tpl/components/location/details.js
@@ -3,17 +3,16 @@
 
 define('components/location/details', ['ajax', 'components/location/charts'], function(ajax, charts) {
     'use strict';
-    
-    var chartsWrapper = document.querySelector('.charts');
+
     var baseUrl = "/rest/locations/%id%/sensors";
-    var interval;
+/*    var interval;*/
     
     /**
      * makes ajax request for sensor data of specific location and builds an access point for menu, to make module loading possible
      * @param   {number} id of the current clicked location in menu
      * @returns {object} Promise
      */
-    function get(id) {
+    function render(id) {
         var url = baseUrl.replace('%id%', id);
         return ajax.get(url, {format: 'json'}).then(function(sensors) {
             return createDetails(sensors);
@@ -26,7 +25,9 @@ define('components/location/details', ['ajax', 'components/location/charts'], fu
      * @returns {object} returns div with all elements to map data
      */
     function createDetails(sensors) {
-        var div = document.createElement('div');
+        // creates container for detail view
+        var detailsDiv = document.createElement('div');
+        detailsDiv.classList.add('details');
         // iterate through all sensors from current location
         for (var i = 0; i < sensors.length; i++) {
             var obj = sensors[i];
@@ -38,25 +39,44 @@ define('components/location/details', ['ajax', 'components/location/charts'], fu
             var figcaption = document.createElement('figcaption');
             // create description with last value and measuring unit
             figcaption.innerHTML = obj.last_action.value + '&nbsp;' + obj.unit;
+            // append them all
             figure.appendChild(img);
             figure.appendChild(figcaption);
-            div.appendChild(figure);
+            detailsDiv.appendChild(figure);
         }
-        // create button for displaying charts
+/*        // create button for displaying charts
         var button = document.createElement('button');
         button.textContent = 'click me';
         // add EventListener for charts and bind sensors to it
-        button.addEventListener('click', clickHandler.bind(button, sensors));
-        div.appendChild(button);
+        button.addEventListener('click', clickHandler.bind(button, sensors, currLi, div));
+        div.appendChild(button);*/
+        // create EventListener to details for displaying charts, bind sensors, current active menupoint and details element to it
+/*        detailsDiv.addEventListener('click', clickHandler.bind(detailsDiv, sensors, currLi, detailsDiv));
+        */
         // returns the div for Promise
-        return div;
+        return detailsDiv;
     }
     
+    /**
+     * clickHandler for charts display button which renders charts every 30 secs
+     * @param {object} sensors json, all sensors from current location
+     * @param {object} event   click event
+     */
+/*    function clickHandler(sensors, currLi, detailsDiv) {
+        renderCharts(sensors, currLi, detailsDiv);
+        // set interval for redraw with new values
+        interval = setInterval(function() {
+            renderCharts(sensors, currLi, detailsDiv);
+        }, 30*1000);
+    }*/
+
     /**
      * first collect all solved promises, then draw charts for every sensor in current location
      * @param {object} sensors current sensor object from location
      */
-    function renderCharts(sensors) {
+ /*   function renderCharts(sensors, currLi, detailsDiv) {
+        var chartsDiv = document.createElement('div');
+        chartsDiv.classList.add('.charts');
         // array for promises
         var promises = [];
         // get the promises for every sensor in location
@@ -65,30 +85,17 @@ define('components/location/details', ['ajax', 'components/location/charts'], fu
         }
         // handle all collected promises at once
         Promise.all(promises).then(function(all) {
-            // clear wrapper
-            chartsWrapper.innerHTML = '';
             // append all returned nodes of the promises in chartwrapper
             for (var i = 0; i < all.length; i++) {
-                chartsWrapper.innerHTML += all[i].innerHTML;
+                chartsDiv.appendChild(all[i]);
             }
+            currLi.removeChild(currLi.detailsDiv);
+            currLi.appendChild(chartsDiv);
         });
-    }
-    
-    /**
-     * clickHandler for charts display button which renders charts every 30 secs
-     * @param {object} sensors json, all sensors from current location
-     * @param {object} event   click event
-     */
-    function clickHandler(sensors, event) {
-        renderCharts(sensors);
-        // set interval for redraw with new values
-        interval = setInterval(function() {
-            renderCharts(sensors);
-        }, 30*1000);
-    }
+    }*/
     
     return {
-        get: get
+        render: render
     };
     
 });
--- homenet/tpl/components/location/list.js
+++ homenet/tpl/components/location/list.js
@@ -1,42 +1,58 @@
 /* jshint browser: true */
 /* jshint esnext: true */
 
-define('components/location/list', ['ajax', 'components/location/details'], function(ajax, details) {
+define('components/location/list', ['ajax', 'components/location/details', 'components/location/charts'], function(ajax, details, charts) {
     'use strict';
 
     var url = "/rest/locations";
-    var wrapper = document.querySelector('.list');
-    var detailsWrapper = document.querySelector('.details');
+    var listNode = document.querySelector('.list');
+/*    var detailsWrapper = document.querySelector('.details');*/
     var interval;
 
-    ajax.get(url, {format: 'json'}).then(function(response) {
-        createMenu(response);
+    // get all locations from rest api and render menu
+    ajax.get(url, {format: 'json'}).then(function(locations) {
+        createMenu(locations);
     });
 
-    function createMenu(response) {
+    /**
+     * creates accordion from all locations
+     * @param {object} locations json object for all locations from rest
+     */
+    function createMenu(locations) {
+        // creates unordered list for accordion
         var ul = document.createElement('ul');
-        for (var i = 0; i < response.length; i++) {
-            var obj = response[i];
+        for (var i = 0; i < locations.length; i++) {
+            var obj = locations[i];
+            // creates li element for each location
             var li = document.createElement('li');
+            // inside li, create a clickable button
             var button = document.createElement('button');
             button.classList.add('list-button');
             button.innerHTML = obj.name;
-            /* bind li element and current object to toggle function */
+            // add eventlistener to button and bind current li and location object to it
             button.addEventListener('click', toggleHandler.bind(button, li, obj));
+            // append them all
             li.appendChild(button);
             ul.appendChild(li);
         }
-        wrapper.appendChild(ul);
+        listNode.appendChild(ul);
     }
 
+    /**
+     * toggles accordion
+     * @param {object} li    current li element
+     * @param {object} obj   current location object
+     * @param {object} event current event
+     */
     function toggleHandler(li, obj, event) {
         // if interval exists, clear
         if (interval) {
             clearInterval(interval);
         }
         var parent = li.parentNode;
+        // find all li elements with class active
         var activeNodes = parent.querySelectorAll('li.active');
-        // if any li has active class and is current li, continue
+        // if founded li is current li, continue
         for (var i = 0; i < activeNodes.length; i++) {
             if (activeNodes[i] === li) {
                 continue;
@@ -44,27 +60,37 @@ define('components/location/list', ['ajax', 'components/location/details'], func
             // else remove active class
             activeNodes[i].classList.remove('active');
         }
-        // and toggle new active class
+        // and toggle active class
         li.classList.toggle('active');
+
         if (li.classList.contains('active')) {
-            li.appendChild(detailsWrapper);
+/*            var li = li;*/
+/*            li.appendChild(detailsWrapper);
             // clear wrapper
             detailsWrapper.innerHTML = '';
-            // append detail view
-            showDetails(obj.id);
+            // append detail view*/
+            showDetails(obj.id, li);
             // setInterval for uptodate sensor data every 10 sec
             interval = setInterval(function() {
-                showDetails(obj.id);
+                showDetails(obj.id, li);
             }, 10*1000);
         } else {
-            li.removeChild(detailsWrapper);
+            for (var j = 0; j < li.childNodes.length; j++) {
+                if (li.childNodes[j].classList.contains('details')) {
+                    li.removeChild(li.childNodes[j]);
+                }
+            }
         }
     }
 
-    function showDetails(id) {
-        details.get(id).then(function(node) {
-            detailsWrapper.innerHTML = '';
-            detailsWrapper.appendChild(node);
+    function showDetails(id, li) {
+        details.render(id).then(function(node) {
+            for (var i = 0; i < li.childNodes.length; i++) {
+                if (li.childNodes[i].classList.contains('details')) {
+                    li.removeChild(li.childNodes[i]);
+                }
+            }
+            li.appendChild(node);
         });
     }
 });
